e8583f0ce1acdbddcd7c5485dee0507f6ca6397d
haphamo <hapham0392@gmail.com>
2020-03-24 16:28:45 -0400

    imported Image from rn, added img component with basic styling

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 
 // flex in react native will default to column instead of row, flex param only supports single num
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World</Text>
+      <Image source={{uri: 'https://randomuser.me/api/portraits/women/60.jpg'}} style={styles.img}/>
     </View>
   )
 }
@@ -19,6 +20,11 @@ const styles = StyleSheet.create({
   text: {
     color: 'pink',
     fontSize: 30,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 50
   }
 })
 export default App;
