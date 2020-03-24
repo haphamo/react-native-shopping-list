1c50cacdd8a1bc3427530c322a5a801e62e8176c
haphamo <hapham0392@gmail.com>
2020-03-24 19:08:23 -0400

    Ran into bug using uuid, tried to use react-native-random-values but code broke. Got code back to before uuid and most importantly, changed build system to legacy build

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native'
-import Header from './components/Header'
+import Header from './components/Header';
+
 // flex in react native will default to column instead of row, flex param only supports single num
 const App = () => {
+  // const [items, setItems] = useState([
+  //   {id: uuid(), text: 'Milk'},
+  //   {id: uuid(), text: 'Eggs'},
+  //   {id: uuid(), text: 'Bread'},
+  //   {id: uuid(), text: 'Juice'}
+  // ])
+
   return (
     <View style={styles.container}>
       <Header />
