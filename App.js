81a95b6428f0e0a4358aeaad79d883f36515c546
haphamo <hapham0392@gmail.com>
2020-03-24 16:32:00 -0400

    removed image, created components dir with Header component

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 // flex in react native will default to column instead of row, flex param only supports single num
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World</Text>
-      <Image source={{uri: 'https://randomuser.me/api/portraits/women/60.jpg'}} style={styles.img}/>
+   
     </View>
   )
 }
@@ -14,17 +14,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
   },
-  text: {
-    color: 'pink',
-    fontSize: 30,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 50
-  }
+
 })
 export default App;
