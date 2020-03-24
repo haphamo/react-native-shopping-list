e852345fa62982b07d260d60b409da72c8e9d967
haphamo <hapham0392@gmail.com>
2020-03-24 16:19:36 -0400

    added flex in-line styling in view component

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native'
 
+// flex in react native will default to column instead of row, flex param only supports single num
 const App = () => {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center'}}>
       <Text>Hello World</Text>
     </View>
   )
